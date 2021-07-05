a6139c6de14ea729a261d94435daf93e405ae668
McAsh09 <201951033@iiitvadodara.ac.in>
2021-07-05 23:12:00 +0530

    path for NewExpenses added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+const App = () => {
+  const expenses = [
+      {
+        id: 'e1',
+        title: 'Washing Powder',
+        amount: 104,
+        date: new Date(2021, 6, 4),
+      },
+
+      {
+        id: 'e2',
+        title: 'New TV',
+        amount: 501,
+        date: new Date(2021, 3, 20),
+      },
+
+      {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 1101,
+        date: new Date(2021, 5, 12),
+      },
+
+      {
+        id: 'e4',
+        title: 'Wooden Desk',
+        amount: 800,
+        date: new Date(2021, 3, 14),
+      }
+  ]
   return (
     <div>
-      <ExpenseItem />
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
